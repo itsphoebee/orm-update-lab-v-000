183eb47fc0bcaea6dd3fa0b17827ae14022e322f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-02 16:45:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
 
   def update
     sql = <<-SQL
-    UPDATE students 
+    UPDATE students
     SET name = ?, grade = ? WHERE id = ?
     SQL
     DB[:conn].execute(sql, self.name, self.grade, self.id) 
